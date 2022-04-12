0247614e595a484fd134c3f8279abc0488e974e9
Otto Cabanillas <oscar.cabanillas@globant.com>
2022-04-12 10:07:50 -0300

    Preparing

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Oscar Cabanillas on 12/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
